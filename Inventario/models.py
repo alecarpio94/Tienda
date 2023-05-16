b818076f800b0f4cc44e37d9a77f1482e5c00962
alecarpio94 <alecarpio94@gmail.com>
2023-05-15 23:01:45 -0500

    Iniciando

---
+++ Inventario/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Categorias(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Unidades(models.Model):
+    Nombre = models.CharField(max_length=50)
+    Cantidad = models.IntegerField()
+    def __str__(self):
+        return self.Nombre
+
+class Productos(models.Model):
+    categoria = models.ManyToManyField(
+        Categorias,
+        default=None,
+    )
+    Nombre = models.CharField(max_length=25)
+    Precio = models.IntegerField()
+    Cantidad = models.IntegerField()
+    Medida = models.ManyToManyField(
+        Unidades,
+        default=None,
+    )
+    def __str__(self):
+        return self.Nombre
---
+++ Venta/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from Inventario.models import Productos
+# Create your models here.
+
+class Cliente(models.Model):
+    Documento = models.IntegerField(primary_key=True)
+    Nombre = models.CharField(max_length=50)
+    Email = models.EmailField()
+    Telefono = models.IntegerField()
+    Direccion = models.CharField(max_length=250)
+    Ciudad = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.Documento)
+
+class Venta(models.Model):
+    id = models.AutoField(primary_key=True)
+    cliente = models.ManyToManyField(
+        Cliente,
+        default=None,
+    )
+    Producto = models.ManyToManyField(
+        Productos,
+        default=None,
+    )
+
+    def __str__(self):
+        return str ("#" + str(self.id))
